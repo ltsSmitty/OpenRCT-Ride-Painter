9e8f9c3b6653211a5617c88c03a942e585c3dfd5
Smitty Penman <smitty.penman@sharpspring.com>
2022-06-09 16:23:28 -0400

    formatting changes by prettier

--- src/controllers/BaseController.ts
+++ src/controllers/BaseController.ts
@@ -1,20 +1,20 @@
-/* eslint-disable max-classes-per-file */
-/* eslint-disable lines-between-class-members */
 import { Store, store, ArrayStore, arrayStore } from "openrct2-flexui";
 import { debug } from "../helpers/logger";
 import PluginNamespace from "../helpers/config";
 
 export default class BaseController<T> {
-    all!: Store<T[]>;
-    selected!: Store<T | null>;
-    selectedIndex!: Store<number>;
-    controllerKeys: { [props: string]: Store<any> };
-    namespaceKey: string;
-    library: any;
+    all!: ArrayStore<T>;
 
-    constructor({ library = [] }: { library: T[] }) {
-        this.library = library;
-        this.initializeController();
+    selected!: Store<T | null>;
+
+    selectedIndex!: Store<number>;
+
+    controllerKeys: { [props: string]: Store<any> };
+
+    namespaceKey: string;
+
+    constructor(all: T[]) {
+        this.initializeController(all);
 
         this.namespaceKey = `${this.constructor.name}`;
         // example controller keys
@@ -24,23 +24,12 @@ export default class BaseController<T> {
         };
     }
 
-    private initializeController(lib = this.library) {
-        this.all = this.updateLibrary(lib);
+    private initializeController(all: T[]) {
+        this.all = arrayStore<T>(all);
         this.selectedIndex = store<number>(0);
         this.selected = store<T | null>(
             this.all.get()[this.selectedIndex.get()]
         );
-    }
-
-    updateLibrary(lib: T[]): Store<T[]> {
-        // if it's possible to add additional themes in the future,
-        // this is where they'll be loaded
-        // todo this might not work?
-        if (!lib) return this.all;
-
-        this.library = lib;
-        this.all = store<T[]>(lib);
-        return this.all;
     }
 
     private getValuesToSave() {
@@ -50,18 +39,37 @@ export default class BaseController<T> {
         return vals;
     }
 
-    saveFeatures() {
+    saveFeatures(storageChoice: "park" | "global" = "park") {
         const valToSave = this.getValuesToSave();
-        context
-            .getParkStorage()
-            .set(`${PluginNamespace}.${this.namespaceKey}`, valToSave);
-        debug(`${this.constructor.name} saved.`);
+        if (storageChoice === "park") {
+            context
+                .getParkStorage()
+                .set(`${PluginNamespace}.${this.namespaceKey}`, valToSave);
+            debug(`${this.constructor.name} saved to park storage.`);
+            return;
+        }
+        context.sharedStorage.set(
+            `${PluginNamespace}.${this.namespaceKey}`,
+            valToSave
+        );
+        debug(`${this.constructor.name} saved to global storage.`);
     }
 
-    loadValuesFromStorage() {
-        const loadedController = context
-            .getParkStorage()
-            .get(`RidePainter.${this.namespaceKey}`) as {
+    loadValuesFromStorage(storageChoice: "park" | "global" = "park") {
+        let loadedController;
+        if (storageChoice === "park") {
+            loadedController = context
+                .getParkStorage()
+                .get(`RidePainter.${this.namespaceKey}`) as {
+                [keys: string]: any;
+            }[];
+            if (!loadedController) return;
+            this.applyValuesFromSave(loadedController);
+            return;
+        }
+        loadedController = context.sharedStorage.get(
+            `RidePainter.${this.namespaceKey}`
+        ) as {
             [keys: string]: any;
         }[];
         if (!loadedController) return;
@@ -69,9 +77,6 @@ export default class BaseController<T> {
     }
 
     private applyValuesFromSave(loadedVals: { [keys: string]: any }[]) {
-        // why do this?
-        this.updateLibrary(this.library);
-
         // re-apply each loaded prop to the right value
         loadedVals.forEach((prop) => {
             debug(
